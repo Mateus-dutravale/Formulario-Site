73854aea7d834fdf6f188c9d42907dfa9aeea43d
unknown <mateus.dutra.vale.dv@gmail.com>
2025-03-20 18:48:32 -0300

    update

---
+++ public/js/register.js
@@ -0,0 +1,6 @@
+
+//breve funçao pra abertura da aba de confirmação de inscrição do formulario
+function abrirConfirmacao(event) {
+    event.preventDefault(); 
+    window.open("../pages/confirmation.html", "_blank"); 
+}
